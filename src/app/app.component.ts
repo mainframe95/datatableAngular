b04726de7f8fc0a082dc1ee91ea82f14c512a7a9
Junior Mensah <jmensah@diagautocenter.net>
2020-06-30 11:00:12 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angulardatatables';
+  // title = 'angulardatatables';
+  public data = [
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+    {name: 'therichpost', email: 'therichpost@gmail.com', website: 'therichpost.com'},
+];
+  dtOptions: DataTables.Settings = {};
+  ngOnInit() {
+      this.dtOptions = {
+        pagingType: 'full_numbers',
+        pageLength: 5,
+        processing: true
+    };
+  }
 }
